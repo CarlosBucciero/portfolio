7d95198d551e3f4e2b7b3bbfc307c7a4fbccf343
“carlosbucciero” <buchiprograma@gmail.com>
2024-12-30 00:09:20 +0100

    Primera subida de portfolio

---
+++ script/script.js
@@ -0,0 +1,6 @@
+function saludar() {
+  var nombre = document.getElementById("nombre").value; 
+  var email = document.getElementById("email").value; 
+  var mensaje = document.getElementById("mensaje").value;   
+  alert("Tu mensaje se ha perdido en la Matrix 🤯");
+}
